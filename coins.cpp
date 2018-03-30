d50e47f6c0ad36ffb3afca8b173b5618f7829d1e
horizon <liudxp@live.cn>
2018-03-30 10:21:43 +0800

    了解数字规律之后的做法

--- coins.cpp
+++ coins.cpp
@@ -1,37 +1,14 @@
 #include <iostream>
-
-#define Int unsigned long long int
-
+#include <cmath>
 using namespace std;
-Int n=0, m=0;
 
-bool is_odd(Int xy){
-    if(xy==1)return true;
-    Int l=1, h=xy, mid=0, s=0;
-    while(l<h){
-        mid=(l+h)/2;
-        s=mid*mid;
-        if(s==xy)
-            return true;
-        else if(s<xy)
-            l=mid+1;
-        else 
-            h=mid-1;
-    }
-    return false;
-}
+
+
 
 int main(){
+    unsigned long long int n, m;
     cin>>n>>m;
-    Int cnt=0;
-    for(Int x=1; x<=n; ++x){
-        for(Int y=1; y<=m; ++y){
-            if(is_odd(x)&is_odd(y))
-                cnt++;
-        }
-    }
-
-    cout<<cnt;
+    cout<<(unsigned long long int)sqrt(n)*(unsigned long long int)sqrt(m);
 
     return 0;
 }
